3791a858a47170d1e3b5f239e9db35edb4b0e950
Kagheni <Kaghenimbale@gmail.com>
2023-11-22 17:04:33 +0200

    Add input field to the library component

--- src/components/library/Library.jsx
+++ src/components/library/Library.jsx
@@ -1,5 +1,58 @@
+import './library.css';
+
 const Library = () => {
-  return <div>Library</div>;
+  return (
+    <div>
+      <div>
+        <form className="forms">
+          <label htmlFor="">
+            <input
+              type="text"
+              className="input_library"
+              name="name"
+              placeholder="Name"
+            />
+          </label>
+          <label htmlFor="">
+            <input
+              type="text"
+              className="input_library"
+              name="class"
+              placeholder="Student Class"
+            />
+          </label>
+          <label htmlFor="">
+            <input
+              type="date"
+              className="input_library"
+              name="datetime"
+              placeholder="Date"
+            />
+          </label>
+          <label htmlFor="">
+            <input
+              type="time"
+              className="input_library"
+              name="name"
+              placeholder="Time"
+            />
+          </label>
+          <label htmlFor="">
+            <input
+              type="tel"
+              className="input_library"
+              name="phone"
+              placeholder="Phone Number"
+            />
+          </label>
+          <button type="submit" className="btn">
+            submit
+          </button>
+        </form>
+      </div>
+      <div className="historics"></div>
+    </div>
+  );
 };
 
 export default Library;
